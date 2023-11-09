82d8d87785c6633e7324c965f087728830198f2c
Fauzan <fauzan@mhs.politala.ac.id>
2023-11-09 11:21:00 +0800

    Menambahkan paragraf pada index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At natus, totam obcaecati quaerat voluptatem cum facere eaque nam inventore hic maiores id assumenda vitae quisquam. Dolores itaque ipsa veritatis placeat.</p>
 </body>
 </html>
